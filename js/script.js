f1e68f5fb03461603f7ba41aa4ba53cf7c0dcf2e
Illia <example@gmail.com>
2023-03-27 17:02:55 +0300

    if else

--- js/script.js
+++ js/script.js
@@ -1,47 +1,35 @@
 /* eslint-disable linebreak-style */
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {
-		'logan': '8.1',
-	},
-	actors: {},
-	genres: [],
-	privat: false
-};
+// if (4 == 9) {
+// 	console.log('Ok!');
+// } else {
+// 	console.log('Error');
+// }
 
+const num = 50;
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-	b = prompt('На сколько оцените его?', ''),
-	c = prompt('Один из последних просмотренных фильмов?', ''),
-	d = prompt('На сколько оцените его?', '');
+// if (num < 49) {
+// 	console.log('Error');
+// } else if (num > 100) {
+// 	console.log ('Много');
+// } else {
+// 	console .log ('Ok!');
+// }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// (num === 50) ? console.log('Ok!') : console.log('Error');
 
-console.log(personalMovieDB);
+switch (num) {
+case 49:
+	console.log('Неверно');
+	break;
+case 100: 
+	console.log('Неверно');
+	break;
+case 50: 
+	console.log('В точку!');
+	break;
+default:
+	console.log('Не в этот раз');
+	break;
+}
