25fc52b7f5856c22b2051432e5e250bfeb7e539d
Illia <example@gmail.com>
2023-03-27 19:56:58 +0300

    lesson nested loops

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 /* eslint-disable linebreak-style */
 'use strict';
 
-let num = 50;
-
-// while (num <= 55) {
-// 	console.log(num);
-// 	num++;
-// }
-
-// do {
-// 	console.log(num);
-// 	num++;
-// }
-// while (num < 55);
-
-for (let i = 1; i < 10; i++) {
-	if (i === 6) {
-		// break;
-		continue;
+first: for (let i = 0; i < 3; i++) {
+	console.log(`First level: ${i}`);
+	for (let j = 0; j < 3; j++) {
+		console.log(`Second level: ${j}`);
+		for (let k = 0; k < 3; k++) {
+			if (k === 2) continue first;
+			console.log(`Third level: ${k}`);
+		}
 	}
-	console.log(i);
 }
+
+// let result = '';
+
+// const length = 7;
+
+// for (let i = 1; i < length; i++){
+
+// 	for (let j = 0; j < i; j++){
+// 		result += '*';
+// 	}
+
+// 	result += '\n';
+// }
+
+// console.log(result);
