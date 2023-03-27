0dcb6ffaa014fdd7f856f0f3e4504689ef847318
Illia <example@gmail.com>
2023-03-27 19:33:35 +0300

    lesson of cycles

--- js/script.js
+++ js/script.js
@@ -1,88 +1,23 @@
 /* eslint-disable linebreak-style */
 'use strict';
 
-// const hamburger = 5;
-// const fries = 0;
+let num = 50;
 
-// if(hamburger && fries) {
-// 	console.log('Я сыт!');
-// } else console.log('Я не сыт!');
-
-// console.log((hamburger && fries));
-
-// const hamburger = 2;
-// const fries = 1;
-// const cola = 1;
-
-// console.log(hamburger === 3 && cola && fries);
-
-// console.log(1 && 0);
-// console.log(1 && 5);
-// console.log(null && 5);
-// console.log(0 && 'review');
-
-// if(hamburger === 3 && cola === 1 && fries) {
-// 	console.log('Все сыты!');
-// } else {
-// 	console.log('Мы уходим');
+// while (num <= 55) {
+// 	console.log(num);
+// 	num++;
 // }
 
-// const hamburger = 3;
-// const fries = 3;
-// const cola = 0;
-// const nuggets =2;
-
-// if(hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-// 	console.log('Все довольны!');
-// } else {
-// 	console.log('Мы уходим');
+// do {
+// 	console.log(num);
+// 	num++;
 // }
+// while (num < 55);
 
-// console.log(hamburger || cola || fries);
-
-// let johnReport, alexReport, samReport, mariaReport = 'done';
-
-// console.log(johnReport || alexReport || samReport || mariaReport);
-
-console.log(NaN || 2 || undefined);
-
-console.log(NaN && 2 && undefined);
-
-console.log(1 && 2 && 3);
-
-console.log(!1 && 2 || !3);
-
-console.log(NaN || null || !3 || undefined || 5);
-
-console.log(NaN || null && !3 && undefined || 5);
-
-console.log(5 === 5 && 3 > 1 || 5);
-
-// const hamburger = 3;
-// const fries = 3;
-// const cola = 0;
-// const nuggets =2;
-
-// if (hamburger === 3 && cola || fries === 3 && nuggets) {
-// 	console.log('Done!');
-// }
-
-// let hamburger;
-// const fries = NaN;
-// const cola = 0;
-// const nuggets = 2;
-
-// if (hamburger || cola || fries === 3 || nuggets) {
-// 	console.log('Done!');
-// }
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-
-if (hamburger && cola || fries === 3 && nuggets) {
-	console.log('Done!');
-} else {
-	console.log('Not');
+for (let i = 1; i < 10; i++) {
+	if (i === 6) {
+		// break;
+		continue;
+	}
+	console.log(i);
 }
