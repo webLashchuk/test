78e8d52c735f6e5ef5bfcc49a5f568185eb45d29
Illia <example@gmail.com>
2023-03-29 17:31:22 +0300

    return function

--- js/script.js
+++ js/script.js
@@ -1,38 +1,28 @@
 /* eslint-disable linebreak-style */
 'use strict';
-// let num = 20;
+const usdCurr = 28;
+const discount = 0.9;
 
-// function showFirstMessage(text) {
-// 	console.log(text);
-// 	num = 30;
-// }
+function convert(amount, curr) {
+	return curr * amount;
+}
 
-// showFirstMessage('Hello world');
-// console.log(num);
+function promotion (result) {
+	console.log(result * discount);
+}
 
-// function calc(a, b) {
-// 	return (a + b);
-// }
+const res = convert(500, usdCurr);
+promotion(res);
 
-// console.log(calc(4, 3));
-// console.log(calc(5, 6));
-// console.log(calc(10, 6));
+function test() {
+	for (let i = 0; i < 5; i++) {
+		console.log(i);
+		if (i === 3) return;
+	}
+	console.log('Done');
+}
 
-// function ret() {
-// 	let num = 50;
-// 	return num;
-// }
+test();
 
-// const anotherNum = ret();
-// console.log(anotherNum);
-
-// const logger = function() {
-// 	console.log('Hello');
-// };
-
-// logger();
-
-const calc = (a, b) =>  {
-	console.log('1');
-	return a + b;
-};
+function doNothing() {}
+console.log(doNothing() === undefined);
