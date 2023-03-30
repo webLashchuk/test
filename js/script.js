51b378a976c32a77ee16e09929af9c0f16d42958
Illia <example@gmail.com>
2023-03-30 17:31:12 +0300

    functions tasks with methods (part1)

--- js/script.js
+++ js/script.js
@@ -1,92 +1,30 @@
 /* eslint-disable linebreak-style */
-/* Задание на урок:
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-
 'use strict';
 
-// Код возьмите из предыдущего домашнего задания
+// function calculateVolumeAndArea(edge) {
+// 	if (typeof(edge) === 'string' || edge <= 0 || edge % 1 != 0) {
+// 		return 'При вычислении произошла ошибка';
+// 	} else {
+// 		const volume = edge * edge * edge;
+// 		const surface = edge * edge * 6;
+// 		return `Объем куба: ${volume}, площадь всей поверхности: ${surface}`;
+// 	}
+// }
 
-let numberOfFilms;
+// calculateVolumeAndArea(12.3);
 
-function start() {
-	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+function getCoupeNumber(place) {
+	if (typeof(place) != 'number' || place < 0 || place % 1 != 0)  {
+		return 'Ошибка. Проверьте правильность введенного номера места';
+	} else if (place > 36 || place === 0) {
+		return 'Таких мест в вагоне не существует';
+	} else {
+		return Math.ceil(place/4);
 	}
+
 }
 
-start();
-
-	
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {
-		'logan': '8.1',
-	},
-	actors: {},
-	genres: [],
-	privat: false
-};
-
-function detectPersonalLevel() {
-	if (personalMovieDB.count < 10) {
-		alert('Просмотрено довольно мало фильмов');
-	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-		alert('Вы классический зритель');
-	} else if (personalMovieDB.count >= 30) {
-		alert('Вы киноман');
-	} 
-}
-
-// detectPersonalLevel();
-
-function rememberMyFilms() {
-	for (let i = 0; i < 2; i++) {
-		const a = prompt('Один из последних просмотренных фильмов?', ''),
-			b = prompt('На сколько оцените его?', ''); 
-		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-			personalMovieDB.movies[a] = b;
-			console.log('Done');
-		}  else {
-			console.log('Ошибка! Вы не ввели данные!');
-			i--;
-		}
-	} 
-}
-
-// rememberMyFilms();
-
-let showMyDB = (hidden) => {
-	if (!hidden)
-		return console.log(personalMovieDB);
-};
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-	for (let i = 1; i <= 3; i++) {
-		// while (c == '' || c == null || c.length > 50 || undefined) {
-		// 	personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`, '');
-		// }
-		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`, '');
-	}
-}
-
-writeYourGenres();
-
-
-    
+getCoupeNumber(13);
             
 
 	
