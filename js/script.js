693a8286a636eef5a7067ae4bd448fdedfa721bc
Illia <example@gmail.com>
2023-03-31 16:33:42 +0300

    functions tasks with methods (part2)

--- js/script.js
+++ js/script.js
@@ -1,31 +1,70 @@
 /* eslint-disable linebreak-style */
 'use strict';
 
-// function calculateVolumeAndArea(edge) {
-// 	if (typeof(edge) === 'string' || edge <= 0 || edge % 1 != 0) {
-// 		return 'При вычислении произошла ошибка';
-// 	} else {
-// 		const volume = edge * edge * edge;
-// 		const surface = edge * edge * 6;
-// 		return `Объем куба: ${volume}, площадь всей поверхности: ${surface}`;
-// 	}
+// function getTimeFromMinutes(minutesTotal) { 
+// 	if (typeof(minutesTotal) !== 'number' || !Number.isInteger(minutesTotal) || minutesTotal < 0) {
+// 		return console.log('Ошибка, проверьте данные');
+// 	} 
+// 	const hours = Math.floor(minutesTotal / 60);
+// 	const minutes = minutesTotal % 60;
+
+// 	let hoursStr = '';
+
+// 	switch (hours) {
+// 	case 0:
+// 		hoursStr = 'часов';
+// 			  break;
+// 	case 1:
+// 		hoursStr = 'час';
+// 			  break;
+// 	case 2:
+// 	case 3:
+// 	case 4:
+// 		hoursStr = 'часа';
+// 			  break;
+// 	default:
+// 		hoursStr = 'часов';
+// 		  }
+
+// 	return console.log(`Это ${hours} ${hoursStr} и ${minutes} минут`);
 // }
 
-// calculateVolumeAndArea(12.3);
+// getTimeFromMinutes(450);
 
-function getCoupeNumber(place) {
-	if (typeof(place) != 'number' || place < 0 || place % 1 != 0)  {
-		return 'Ошибка. Проверьте правильность введенного номера места';
-	} else if (place > 36 || place === 0) {
-		return 'Таких мест в вагоне не существует';
+function findMaxNumber(firstVal, secondVal, thirdVal, fourVal) {
+	if (typeof(firstVal) !='number' || typeof(secondVal) !='number' || typeof(thirdVal) !='number' || typeof(fourVal) !='number') {
+		return console.log (0);
 	} else {
-		return Math.ceil(place/4);
+		let result = 0;
+		if (firstVal > secondVal && firstVal > thirdVal && firstVal > fourVal) {
+			result = firstVal;
+		} else if (secondVal > firstVal && secondVal > thirdVal && secondVal > fourVal) {
+			result = secondVal;
+		} else if (thirdVal > firstVal && thirdVal > secondVal && thirdVal > fourVal) {
+			result = thirdVal;
+		} else {
+			result = fourVal;
+		}
+		return console.log(result);
 	}
-
 }
 
-getCoupeNumber(13);
-            
+// findMaxNumber(1, 5, 6.6, 11);
 
+
+// function findMaxNumber(a, b ,c, d) {
+//     // Самое простое - это использовать Math.max :)
+//     // А оптимизировать такую проверку мы научимся совсем скоро
+//     if (typeof(a) !== 'number' ||
+//         typeof(b) !== 'number' ||
+//         typeof(c) !== 'number' ||
+//         typeof(d) !== 'number') {
+//         return 0;
+//     } else {
+//         return Math.max(a, b ,c, d);
+//     }
+// }
+
+// findMaxNumber(1, 5, 6.6, 11);
 	
 	
