7025adac6f97e61d9ca892eeb75f841a5fed2bc8
Illia <example@gmail.com>
2023-04-01 15:53:08 +0300

    objects

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
 /* eslint-disable linebreak-style */
 'use strict';
 
-// function first() {
-// 	//Do something
-// 	setTimeout(function() {
-// 		console.log(1);
-// 	}, 500);
+const options = {
+	name: 'test',
+	width: 1024,
+	height: 1024,
+	colors: {
+		border: 'black',
+		bg: 'red'
+	},
+	makeTest: function() {
+		console.log('test');
+	}
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+// 	if (typeof(options[key]) === 'object') {
+// 		for (let val in options[key]) {
+// 			console.log(`Свойство ${val} имеет значение ${options[key][val]}`);
+// 			// counter++;
+// 		}
+// 	} else {
+// 		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+// 		counter++;
+// 	}
 // }
+// console.log(counter);
 
-// function second() {
-// 	console.log(2);
-// }
-
-// first();
-// second();
-
-function learnJS(lang, callback) {
-	console.log(`Я учу: ${lang}`);
-	callback();
-}
-
-function done() {
-	console.log('Я прошел этот урок');
-}
-
-learnJS('JavaScript', done);
+// console.log(Object.keys(options).length);
