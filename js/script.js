d1c9fa8fb1eda4445eabdca89eb629b876237161
Illia <example@gmail.com>
2023-04-06 23:48:50 +0300

    arrays task (part2)

--- js/script.js
+++ js/script.js
@@ -1,122 +1,38 @@
 /* eslint-disable linebreak-style */
 'use strict';
+//ЗАДАЧА НА СОБЕСЕДОВАНИЕ
+// const someString = 'This is some strange string';
 
-// let a = 5,
-// 	b = a;
-
-// b = b + 5;
-
-// console.log(b);
-// console.log(a);
-
-// const obj = {
-// 	a: 5,
-// 	b: 1
-// };
-
-// const copy = obj; //ссылку
-
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-// function copy(mainObj) {
-// 	let objCopy = {};
-
-// 	let key;
-// 	for(key in mainObj) {
-// 		objCopy[key] = mainObj[key];
+// function reverse(str) {
+// 	let stringArr = [];
+// 	if(typeof(str) != 'string') {
+// 		return console.log('Ошибка!');
+// 	} else {
+// 		for (let i = 0; i <= str.length -1; i++) {
+// 			stringArr[i] = str[i];
+// 		};
+// 		console.log(stringArr.reverse().join(''));
 // 	}
-
-// 	return objCopy;
 // }
 
-// const numbers = {
-// 	a: 2,
-// 	b: 5,
-// 	c: {
-// 		x: 7,
-// 		y:4
-// 	}
-// };
+// reverse(someString);
 
-// const newNumbers = copy(numbers);
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
-// newNumbers.a = 10;
-// newNumbers.c.x = 10;
-
-// // console.log(newNumbers);
-// // console.log(numbers);
-
-// const add = {
-// 	d: 17,
-// 	e: 20
-// };
-
-// const clone = Object.assign({}, add);
-
-// clone.d = 20;
-
-// // console.log(add);
-// // console.log(clone);
-
-// const oldArray = ['a', 'b', 'c'];
-// const newArray = oldArray.slice();
-
-// newArray[1] = 'adadadasas';
-// console.log(newArray);
-// console.log(oldArray);
-
-// const video = ['youtube', 'vimeo', 'rutube'],
-// 	blogs = ['wordpress', 'livejournal', 'blogger'],
-// 	internet = [...video, ...blogs, 'vk', 'facebook'];
-
-// console.log(internet);
-
-// function log(a, b, c) {
-// 	console.log(a);
-// 	console.log(b);
-// 	console.log(c);
-// }
-
-// const num = [2, 5, 7];
-
-// log(...num);
-
-// const array = ['a', 'b'];
-
-// const newAarray = [...array];
-
-// const q = {
-// 	one: 1,
-// 	two: 2
-// };
-
-// const newObj = {...q};
-
-const family = ['Peter', 'Ann', 'Alex', 'Linda'];
-
-function showFamily(arr) {
-	let str = '';
-	if(arr.length <= 0) {
-		return console.log('Семья пуста');
+function availableCurr(arr, missingCurr) {
+	if(arr.length === 0) {
+		return 'Нет доступных валют';
 	} else {
+		arr = baseCurrencies.concat(additionalCurrencies);
+		let str = '';
 		arr.forEach(function(item) {
-			str += ` ${item}`;
+		// console.log(item);
+			if (item != missingCurr) 
+		    str += `${item}\n`;
 		});
-		return console.log(`Семья состоит из:${str}`);
+		return `Доступные валюты:\n${str}`;
 	}
 }
 
-showFamily(family);
-
-const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
-
-function standardizeStrings(arr) {
-	for (let value of arr) {
-		console.log(value.toLowerCase());
-	}
-}
-
-standardizeStrings(favoriteCities);
+availableCurr(arr, missingCurr);
