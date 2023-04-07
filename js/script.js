a0f4b3c2cce30839487dd6b6a107e4dfa3f7dfdb
Illia <example@gmail.com>
2023-04-07 15:50:07 +0300

    Basics of object prototyping

--- js/script.js
+++ js/script.js
@@ -1,38 +1,30 @@
 /* eslint-disable linebreak-style */
 'use strict';
-//ЗАДАЧА НА СОБЕСЕДОВАНИЕ
-// const someString = 'This is some strange string';
 
-// function reverse(str) {
-// 	let stringArr = [];
-// 	if(typeof(str) != 'string') {
-// 		return console.log('Ошибка!');
-// 	} else {
-// 		for (let i = 0; i <= str.length -1; i++) {
-// 			stringArr[i] = str[i];
-// 		};
-// 		console.log(stringArr.reverse().join(''));
-// 	}
-// }
+let str = 'some';
+let strObj = new String(str);
 
-// reverse(someString);
+console.log(typeof(str));
+console.log(typeof(strObj));
 
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+console.dir([1,2,3]);
 
-function availableCurr(arr, missingCurr) {
-	if(arr.length === 0) {
-		return 'Нет доступных валют';
-	} else {
-		arr = baseCurrencies.concat(additionalCurrencies);
-		let str = '';
-		arr.forEach(function(item) {
-		// console.log(item);
-			if (item != missingCurr) 
-		    str += `${item}\n`;
-		});
-		return `Доступные валюты:\n${str}`;
+const soldier = {
+	health: 400,
+	armor: 100,
+	sayHello: function() {
+		console.log('Hello');
 	}
-}
+};
 
-availableCurr(arr, missingCurr);
+const john = Object.create(soldier);
+
+// const john = {
+// 	health: 100
+// };
+
+// john.__proto__ = soldier;
+
+// Object.setPrototypeOf(john, soldier);
+
+john.sayHello();
