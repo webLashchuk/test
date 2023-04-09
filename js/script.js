5cf7642b2ec7e2afd3caf8a4d394358bdec69083
Illia <example@gmail.com>
2023-04-09 13:21:06 +0300

    arrays task *

--- js/script.js
+++ js/script.js
@@ -1,87 +1,43 @@
 /* eslint-disable linebreak-style */
-/* Задание на урок:
-
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
-
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
-
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
-
 'use strict';
-	
-const personalMovieDB = {
-	count: 0,
-	movies: {
-		'logan': '8.1',
-	},
-	actors: {},
-	genres: [],
-	privat: false,
-	start: function() {
-		personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-	
-		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-			personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const shoppingMallData = {
+	shops: [
+		{
+			width: 10,
+			length: 5
+		},
+		{
+			width: 15,
+			length: 7
+		},
+		{
+			width: 20,
+			length: 5
+		},
+		{
+			width: 8,
+			length: 10
 		}
-	},
-	detectPersonalLevel: function() {
-		if (personalMovieDB.count < 10) {
-			alert('Просмотрено довольно мало фильмов');
-		} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-			alert('Вы классический зритель');
-		} else if (personalMovieDB.count >= 30) {
-			alert('Вы киноман');
-		} 
-	},
-	rememberMyFilms: function() {
-		for (let i = 0; i < 2; i++) {
-			const a = prompt('Один из последних просмотренных фильмов?', ''),
-				b = prompt('На сколько оцените его?', ''); 
-			if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-				personalMovieDB.movies[a] = b;
-				console.log('Done');
-			}  else {
-				console.log('Ошибка! Вы не ввели данные!');
-				i--;
-			}
-		} 
-	},
-	showMyDB: (hidden) => {
-		if (!hidden)
-			return console.log(personalMovieDB);
-	},
-	toggleVisibleMyDB: function() {
-		if (personalMovieDB.privat) {
-			personalMovieDB.privat = false;
-		} else {
-			personalMovieDB.privat = true;
-		}
-	},
-	writeYourGenres: function() {
-		for (let i = 1; i <= 3; i++) {
-			let genre = prompt(`Ваш любимый жанр под номером ${i}?`, '');
-			if (genre === '' || genre === null) {
-				genre = prompt(`Ваш любимый жанр под номером ${i}?`, '');
-				i--;
-			} else {
-				personalMovieDB.genres[i - 1] = genre;
-			}
-		}
-		personalMovieDB.genres.forEach((item, i) => {
-			console.log(`Любимый жанр ${i + 1} - это ${item}`);
-		});
-	}
+	],
+	height: 5,
+	moneyPer1m3: 30,
+	budget: 50000
 };
 
-personalMovieDB.start(personalMovieDB);
-personalMovieDB.detectPersonalLevel(personalMovieDB);
-personalMovieDB.rememberMyFilms(personalMovieDB);
-personalMovieDB.showMyDB(personalMovieDB);
-personalMovieDB.toggleVisibleMyDB(personalMovieDB);
-personalMovieDB.writeYourGenres(personalMovieDB);
+function isBudgetEnough(data) {
+	let totalBudget = 0;
+	data.shops.forEach(function(item) {
+		totalBudget  += item.width * item.length;
+	});
+	totalBudget = totalBudget * data.height * data.moneyPer1m3;
+	if (totalBudget <= data.budget) {
+		console.log('Бюджета достаточно');
+	} else if (totalBudget > data.budget) {
+		console.log('Бюджета недостаточно');
+	} else {
+		console.log('Бюджета достаточно');
+	}
+}
+
+isBudgetEnough(shoppingMallData);
