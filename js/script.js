6a676091a22309613f2152fa556d44c2c36f9463
Illia <example@gmail.com>
2023-04-12 17:07:09 +0300

    arrays task*

--- js/script.js
+++ js/script.js
@@ -1,43 +1,26 @@
 /* eslint-disable linebreak-style */
 'use strict';
 
-const shoppingMallData = {
-	shops: [
-		{
-			width: 10,
-			length: 5
-		},
-		{
-			width: 15,
-			length: 7
-		},
-		{
-			width: 20,
-			length: 5
-		},
-		{
-			width: 8,
-			length: 10
-		}
-	],
-	height: 5,
-	moneyPer1m3: 30,
-	budget: 50000
-};
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
-function isBudgetEnough(data) {
-	let totalBudget = 0;
-	data.shops.forEach(function(item) {
-		totalBudget  += item.width * item.length;
-	});
-	totalBudget = totalBudget * data.height * data.moneyPer1m3;
-	if (totalBudget <= data.budget) {
-		console.log('Бюджета достаточно');
-	} else if (totalBudget > data.budget) {
-		console.log('Бюджета недостаточно');
-	} else {
-		console.log('Бюджета достаточно');
+function sortStudentsByGroups(arr) {
+	arr.sort();
+	let a = [], b = [], c = [], rest = []; 
+	for (let i = 0; i <= arr.length; i++) {
+		if (i < 3) {
+			a.push(arr[i]);
+		} else if (i < 6) {
+			b.push(arr[i]);
+		} else if (i < 9) {
+			c.push(arr[i]);
+		} else {
+			rest.push(arr[i]);
+		}
 	}
+	return console.log(a, b,  c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`);
+	
 }
 
-isBudgetEnough(shoppingMallData);
+sortStudentsByGroups(students);
+
+// КОГДА ПРИДЕШЬ ПОПРОБУЙ ЧЕРЕЗ SPREAD ОПЕРАТОР (СМОТРИ В ЗАПИСЯХ)
